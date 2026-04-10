bdac17423adf9dfb59c3cf768e04863ee42148a8
indrasensingh823 <indrasensingh823gkp@gmail.com>
2026-04-10 20:41:41 +0530

    Fix Issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState, useEffect, useRef } from 'react';
-import Loader from "./components/Loader";
+import Loader from "./components/Loader.js";
 import io from 'socket.io-client';
 import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation, Link } from 'react-router-dom';
 import About from './components/About.js';
