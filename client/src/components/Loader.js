ab0375e0558d77c5259644116c56d447a2dc6196
indrasensingh823 <indrasensingh823gkp@gmail.com>
2026-04-11 13:02:43 +0530

    Update Loader

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -4,16 +4,30 @@ import "./Loader.css";
 const Loader = () => {
   return (
     <div className="loader-container">
+      {/* 🌌 Background particles */}
+      <div className="particles"></div>
+
       <div className="loader-content">
         <img src="/logo.png" alt="ChatterLink Logo" className="loader-logo" />
 
         <h1 className="loader-text">
-          <span>C</span><span>h</span><span>a</span><span>t</span>
-          <span>t</span><span>e</span><span>r</span>
-          <span>L</span><span>i</span><span>n</span><span>k</span>
+          {"ChatterLink".split("").map((char, i) => (
+            <span key={i}>{char}</span>
+          ))}
         </h1>
 
-        <p className="loader-tagline">Connect • Chat • Collaborate</p>
+        <p className="loader-tagline">
+          🚀 Welcome to the future of communication
+        </p>
+
+        <p className="loader-subtext">
+          Connecting you with the world in real-time...
+        </p>
+
+        {/* 🔄 Loading Bar */}
+        <div className="loader-bar">
+          <div className="loader-progress"></div>
+        </div>
       </div>
     </div>
   );
